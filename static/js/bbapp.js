688d60c18d5e0cbe5e08fe221c43b9afb8e91e55
Sam Wimberly <samwimb02@gmail.com>
2019-06-27 18:33:52 -0400

    "Added heat gauge next to the pie chart. Created in the metaData function."

--- static/js/bbapp.js
+++ static/js/bbapp.js
@@ -9,6 +9,7 @@ function buildMetadata(sample) {
     // Use d3 to select the panel with id of `#sample-metadata`
 
     var md_panel = d3.select("#sample-metadata");
+    var MetaData = []
 
     // Use `.html("") to clear any existing metadata
     md_panel.html("");
@@ -17,12 +18,95 @@ function buildMetadata(sample) {
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
     Object.entries(sampleData).forEach(([key, value]) => {
+      MetaData.push([key, value])
       md_panel.append('h6').text(`${key}, ${value}`);
     })
 
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
+      var level = parseFloat(MetaData[5][1])*18;
+      console.log(level);
+  
+      // Trig to calc meter point
+      var degrees = 171 - level,
+          radius = .5;
+          console.log(degrees)
+      var radians = (degrees * Math.PI) / 180;
+      var x = radius * Math.cos(radians);
+      var y = radius * Math.sin(radians);
+  
+      // Path: may have to change to create a better triangle
+      var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+          pathX = String(x),
+          space = ' ',
+          pathY = String(y),
+          pathEnd = ' Z';
+      var path = mainPath.concat(pathX,space,pathY,pathEnd);
+  
+      var data = [
+      { 
+        type: 'scatter',
+        x:[0], 
+        y:[0],
+        marker: {size: 12, color:'850000'},
+        showlegend: false,
+        name: 'Freq',
+        text: level,
+        hoverinfo: 'text+name'
+      },
+      { 
+        values: [50/10, 50/10, 50/10, 50/10, 50/10, 50/10, 50/10, 50/10, 50/10, 50/10, 50],
+        rotation: 90,
+        text: ["9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ""],
+        textinfo: 'text',
+        textposition:'inside',
+        marker: {colors:[
+          "rgba(0, 105, 11, .5)",
+          "rgba(0, 105, 11, .5)",
+          "rgba(10, 120, 22, .5)",
+          "rgba(14, 127, 0, .5)",
+          "rgba(110, 154, 22, .5)",
+          "rgba(170, 202, 42, .5)",
+          "rgba(202, 209, 95, .5)",
+          "rgba(210, 206, 145, .5)",
+          "rgba(232, 226, 202, .5)",
+          "rgba(240, 230, 215, .5)",
+          "rgba(255, 255, 255, 0)"
+        ]
+      },
+      labels: ["9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ""],
+      hoverinfo: 'label',
+      hole: .5,
+      type: 'pie',
+      showlegend: false
+      }];
+  
+      var layout = {
+      shapes:[{
+          type: 'path',
+          path: path,
+          fillcolor: '850000',
+          line: {
+            color: '850000'
+          }
+        }],
+      title: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week",
+      height: 457.5,
+      width: 450,
+      xaxis: {
+        zeroline:false, 
+        showticklabels:false,
+        showgrid: false, 
+        range: [-1, 1]},
+      yaxis: {
+        zeroline:false, 
+        showticklabels:false,
+        showgrid: false, 
+        range: [-1, 1]}
+      };
+  
+      Plotly.newPlot('gauge', data, layout);
   })}
+
+    // BONUS: Build the Gauge Chart
 
 function buildCharts(sample) {
 
@@ -93,6 +177,7 @@ function init() {
     const firstSample = sampleNames[0];
     buildCharts(firstSample);
     buildMetadata(firstSample);
+    // buildGauge(firstSample);
   });
 }
 
@@ -100,6 +185,7 @@ function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
   buildCharts(newSample);
   buildMetadata(newSample);
+  // buildGauge(newSample);
 }
 
 // Initialize the dashboard
